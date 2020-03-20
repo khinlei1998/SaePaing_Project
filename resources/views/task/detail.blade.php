a81fb8b326480057efdd9131d2d0cc842b3a9c27
Su Yee Myaing <suyeemyaing@ritz.com.mm>
2020-03-20 10:34:46 +0630

    solved the problem public/app

--- resources/views/task/detail.blade.php
+++ resources/views/task/detail.blade.php
@@ -248,6 +248,49 @@
                             <hr>
                         </div>
 
+                        <!-- Test -->
+                        <div class="modal fade" id="hod_report_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                                                        <div class="modal-dialog modal-lg" role="document">
+                                                                            <div class="modal-content">
+                                                                                <div class="modal-header">
+                                                                                    <h5 class="modal-title" id="exampleModalLongTitle"><i class="fa fa-info-circle pr-3" ></i><p class="d-inline-block" id="hot_report_title"></p></h5>
+
+                                                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                                                <span aria-hidden="true">&times;</span>
+                                                                                            </button>
+                                                                                </div>
+                                                                                <div class="modal-body">
+                                                                                    <div class="form-group">
+                                                                                        <label for="message-text" class="col-form-label">Report Letter:</label>
+                                                                                        <textarea class="form-control" id="report_text"></textarea>
+                                                                                    </div>
+                                                                                </div>
+                                                                                <div class="modal-body">
+                                                                                    <div class="form-group">
+                                                                                        <label for="message-text" class="col-form-label">Percentage:</label>
+                                                                                        <input type="number" class="form-control" id="per" min="0" max="100"></input>
+                                                                                    </div>
+                                                                                </div>
+                                                                                <div class="modal-footer">
+                                                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                                                    <button type="button" class="btn btn-primary" id="hod_report_submit">Report</button>
+                                                                                </div>
+
+                                                                                <div class="col-sm-12 no-gutters p-5 test-toast">
+                                                                                    <div id="tui-image-editor-container" class="shadow-lg rounded"></div>
+                                                                                  <button class="btn btn-success" onclick="savedatatosaver()">Save</button>
+                                                                                </div>
+
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+
+                                                                    <div class="col-md-2 pl-3 h4">
+                                                                                                    <button type="button" class="btn btn-success btn_hot_report">Report</button>
+                                                                                                </div>
+
+                        <!-- Test End -->
+
                         <div class="container pb-3">
                             <div class="row">
                                 
@@ -289,9 +332,123 @@
     </div>
 @endsection
 @push('scripts')
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
     <script>
         window.onload = function () {
             $(function () {
+
+                // test
+                $('.btn_hot_report').click(function(){
+                   $('#hod_report_modal').modal('show');
+                });
+
+                var imageEditor;
+
+      
+            // Image editor
+            imageEditor = new tui.ImageEditor('#tui-image-editor-container', {
+
+                includeUI: {
+                    loadImage: {
+                        path: 'storage/profile/default.jpg',
+                        name: 'SampleImage'
+                    },
+                    download: false,
+                    theme: blackTheme, // or whiteTheme
+                    initMenu: 'filter',
+                    uiSize: {
+                        height: '580px',
+                        width: '100%'
+                    },
+                    menuBarPosition: 'left',
+                    cssMaxWidth: 200,
+                    usageStatistics: false,
+                },
+
+
+
+
+
+            }, {
+                methods: {
+                    selectImage: function (event) {
+                        console.log('fff');
+                    },
+                    crop: function () {
+                        console.log('ffff');
+                    }
+
+                }
+            });
+
+            window.onresize = function () {
+
+                imageEditor.ui.resizeEditor();
+            }
+            imageEditor.on('mousedown', function (pos) {
+                console.log('ppppp')
+            });
+            // imageEditor.on('applyFilter', function(pos) {
+            //     console.log('ppppp')
+            // });
+
+      
+
+
+       //saving function to saver
+
+        var image;
+        function savedatatosaver() {
+
+
+           // let tets=imageEditor.getImageName();//test use for instance methods
+
+
+
+           // console.log(tets);
+            image=imageEditor.toDataURL();
+            image = image.replace('data:image/png;base64,', '');
+
+
+
+
+            // console.log(imageEditor.toDataURL())///this line is important to save image in server
+            //save image to server
+            // $.ajax({
+            //     type: 'POST',
+            //     url: 'Default.aspx/MoveImages',
+            //     data: '{ "imageData" : "' + image + '" }',
+            //     contentType: 'application/json; charset=utf-8',
+            //     dataType: 'json',
+            //     success: function (msg) {
+            //     }
+            // });
+
+
+            $.ajax({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                method: "POST",
+                contentType: 'application/json; charset=utf-8',
+
+                url: "/saveimagetoserver",
+                data:'{ "imageData" : "' + image + '" }',
+            }).done(function (data) {
+
+                console.log("S blade: [task/create] component :[employee dropdown] from:app.js Data => Employee count" +data.success );
+
+            }).fail(function (jqXHR, textStatcbp_listus) {
+                console.log("F blade: [task/create] component :[department dropdown] from:app.js Fail =>" )
+            });
+        }
+
+                // test end
+
+
+
+
+
            var last_status=$(".status").val();
            
             if( last_status==3){
