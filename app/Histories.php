7f5fb84dc4c08da39420ec4a9654012699cd5775
Su Yee Myaing <suyeemyaing@ritz.com.mm>
2020-03-21 13:23:59 +0630

    update histories model

--- app/Histories.php
+++ app/Histories.php
@@ -9,14 +9,14 @@ class Histories extends Model
 {
     //
     protected $table='Histories';
-    protected $fillable=['sender_id','receiver_id','description','read_this','project_id','cbp_id','human_date','link_name'];
+    protected $fillable=['sender_id','receiver_id','description','read_this','project_id','cbp_id','link_name'];
     public function getDateFormat()
     {
         return 'Y-m-d H:i:s.u';
     }
-    public function getHumanDateAttribute()
-    {
-        $hd= Carbon::parse($this->created_at)->isoFormat('DD. MMMM YYYY');
-        return $this->attributes['human_date'] == $hd;
-    }
+    // public function getHumanDateAttribute()
+    // {
+    //     $hd= Carbon::parse($this->created_at)->isoFormat('DD. MMMM YYYY');
+    //     return $this->attributes['human_date'] == $hd;
+    // }
 }
